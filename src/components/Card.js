701ae6a63a54f72d9359283f7d55d8e6e9ab284b
Hannah Akhtar <hannahaakhtar@gmail.com>
2021-05-21 15:46:56 +0100

    loader added whilst state updating

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
-export default function Card({ cityName, windSpeed, windDirection, Humidity }) {
+export default function Card({ imageLink, cityName, windSpeed, windDirection, windCompass, Humidity, Visibility, airPressure, minTemp, maxTemp }) {
   return (
     <div className="card">
       <div className="card-image">
-        <img href={"https://www.metaweather.com/static/img/weather/png/sn.png"} alt={`View of current weather state in ${cityName}`}/>
+        <img href={imageLink} alt={`View of current weather state in ${cityName}`} />
       </div>
       <div className="card-content">
         <h2>{cityName}</h2>
       </div>
       <div className="content">
         <p>Wind speed: {windSpeed.toFixed(0)} mph</p>
-        <p>Wind direction: {windDirection} degrees</p>
+        <p>Wind direction: {windDirection.toFixed(0)} degrees {windCompass}</p>
         <p>Humidity: {Humidity}%</p>
+        <p>Visibility: {Visibility.toFixed(0)} miles</p>
+        <p>Air Pressure: {airPressure.toFixed(0)} mbar</p>
+        <p>Temperature range (min-max): {minTemp.toFixed(0)} - {maxTemp.toFixed(0)} degrees</p>
       </div>
     </div>
   )
--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import Loader from "react-loader-spinner"
+
 import Navbar from '../components/Navbar'
 import Card from '../components/Card'
-
-// ? need loader for loading state
 
 export default function Search() {
   const [search, setSearch] = useState('')
@@ -13,41 +13,62 @@ export default function Search() {
   const [userSubmitted, setUserSubmitted] = useState(false)
   const [woeid, setWoeid] = useState('')
   const [displayCard, setDisplayCard] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   async function handleSubmit() {
+    setLoading(true)
     const { data } = await axios.get(`https://stormy-atoll-29846.herokuapp.com/metaweather.com/api/location/search/?query=${search}`)
+    console.log(data)
     setSearchResults(data)
     setUserSubmitted(true)
+    if (data.length === 0) {
+      setLoading(false)
+    }
     if (data.length === 1) {
       setWoeid(data[0].woeid)
-    }
-    if (data.length > 1) {
-      setDisplayCard(false)
     }
   }
 
   useEffect(() => {
     async function fetchAllLocationData() {
-        const { data } = await axios.get(`https://stormy-atoll-29846.herokuapp.com/metaweather.com/api/location/${woeid}`)
-        setAllLocationData(data)
-        setDisplayCard(true)
+      const { data } = await axios.get(`https://stormy-atoll-29846.herokuapp.com/metaweather.com/api/location/${woeid}`)
+      setAllLocationData(data)
+      console.log(data)
+      setDisplayCard(true)
+      setLoading(false)
     }
     fetchAllLocationData()
-
   }, [woeid])
 
+  if (loading) {
+    return (
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+      // timeout={2000} //3 secs
+      />
+    )
+  }
+
   let results
+
   if (searchResults.length === 0 && userSubmitted) {
     results = <p>No results - search again?</p>
   } else if (allLocationData && userSubmitted && displayCard) {
     results = <>
       <Card
-      // ? image isn't working
-        weatherStateAbbreviation={allLocationData.consolidated_weather[0].weather_state_abbr}
+        weatherStateAbbreviation={`https://www.metaweather.com/static/img/weather/png/${allLocationData.consolidated_weather[0].weather_state_abbr}.png`}
         cityName={allLocationData.title}
         windSpeed={allLocationData.consolidated_weather[0].wind_speed}
         windDirection={allLocationData.consolidated_weather[0].wind_direction}
+        windCompass={allLocationData.consolidated_weather[0].wind_direction_compass}
         Humidity={allLocationData.consolidated_weather[0].humidity}
+        Visibility={allLocationData.consolidated_weather[0].visibility}
+        airPressure={allLocationData.consolidated_weather[0].air_pressure}
+        minTemp={allLocationData.consolidated_weather[0].min_temp}
+        maxTemp={allLocationData.consolidated_weather[0].max_temp}
       />
     </>
   } else if (searchResults.length > 1 && userSubmitted) {
@@ -63,7 +84,7 @@ export default function Search() {
   return (
     <>
       <Navbar />
-      <h1>Search! 🌞</h1>
+      <h1>Search</h1>
       <input className="input" type="text" placeholder="Search here" onChange={(e) => setSearch(e.target.value)} />
       <button className="submit" onClick={handleSubmit}>Search</button>
       {results}
